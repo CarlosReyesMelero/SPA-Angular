3c9186fa7acf404bd64ffca7004613a26f402453
CarlosReyesMelero <creyesmelero@gmail.com>
2024-07-28 20:08:23 +0200

    Countries Routing Module and creation of search components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
 
 
 
@@ -10,13 +14,18 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
   declarations: [
     AboutPageComponent,
     HomePageComponent,
+    SidebarComponent,
+    ContactPageComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     AboutPageComponent,
     HomePageComponent,
+    SidebarComponent,
+    ContactPageComponent
   ]
 })
 export class SharedModule { }
